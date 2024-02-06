2d757bd76d99f25fb12750e7de30a7fa7a519a79
MariaLimon <limon185mari@gmail.com>
2024-02-06 08:28:40 -0700

    Agregar archivos de proyecto.

---
+++ SplashScreen_MCLG/SplashScreen_MCLG.Android/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SplashScreen_MCLG.Droid
+{
+	[Activity(Label = "SplashScreen")]
+	public class SplashScreen : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+			// Create your application here
+		}
+	}
+}
